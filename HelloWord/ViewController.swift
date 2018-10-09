a902f05a26647a256dd698588f0d29e436a56dce
山中敬人 <yamanakayoshito@yamanakayoshitonomacbook-puro.local>
2018-10-09 16:24:00 +0900

    Initial Commit

---
+++ HelloWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWord
+//
+//  Created by 山中敬人 on 2018/10/09.
+//  Copyright © 2018年 hayato.yamanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
